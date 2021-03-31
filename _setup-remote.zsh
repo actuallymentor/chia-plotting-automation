7ca3adeb0a1f3c86aeabf87fd08cc453aaf22c2e
Mentor Palokaj <mentor@palokaj.co>
2021-03-31 15:42:33 +0200

    Working remote setup

--- _setup-remote.zsh
+++ _setup-remote.zsh
@@ -1,7 +1,11 @@
-echo "Setting up $1" && \
+#!/bin/zsh
+
+# Assumption in script: you want the private key at the remote in ~/chiafarmer
+
+echo "Setting up $1 with ${0:a:h}/.env " && \
 source "${0:a:h}/.env" && \
 ssh-keyscan $1 >> ~/.ssh/known_hosts && \
-rsync -zvP $offlinesshkey root@$1:$offlinesshkey && \
+rsync -zvP $offlinesshkey root@$1:~/chiafarmer && \
 rsync -zvP "${0:a:h}/.env" root@$1:~/.env && \
-ssh -t root@$1 "git clone git clone https://github.com/actuallymentor/chia-plotting-automation.git && cd chia-plotting-automation && echo 'Ready to bash install.sh && nohup zsh ./everplot.zsh & disown'"
+ssh -t root@$1 "git clone https://github.com/actuallymentor/chia-plotting-automation.git && cd chia-plotting-automation && echo 'Ready to bash install.sh && nohup zsh ./everplot.zsh & disown' && /bin/bash -i"
 echo "Something went wrong"
