b1c9eb2930f13088913bb4c10db87ca0af5ec1fe
Mentor <mentor@palokaj.co>
2021-04-10 14:21:32 +0200

    Priority pushover 0

--- functions/push.zsh
+++ functions/push.zsh
@@ -8,7 +8,7 @@ function push() {
 	# Pushover validation
 	if [ -v pushover_user ] && [ -v pushover_token ]; then
 		curl -f -X POST \
-		-d "token=$pushover_token&user=$pushover_user&title=$1&message=$2 at $( hostname )/$myip&url=&priority=1" \
+		-d "token=$pushover_token&user=$pushover_user&title=$1&message=$2 at $( hostname )/$myip&url=&priority=0" \
 		https://api.pushover.net/1/messages.json
 	else
 		echo "[ $( date ) ] [ push.zsh ] No push tokens, only writing to log" >> $logfile
