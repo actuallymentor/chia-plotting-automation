2a85cd1c6c88ceaa1c2fcb6fe0c409fea8782598
Mentor <mentor@palokaj.co>
2021-06-07 12:41:26 +0200

    Syntax fix

--- _update-and-restart-all-uploads.zsh
+++ _update-and-restart-all-uploads.zsh
@@ -5,20 +5,25 @@ oldUploadPath=$1
 newUploadPath=$2
 
 # Get all ips
+cd digital-ocean
 ips=$( npm run getdroplets | grep -o "[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" )
+cd ..
 
 # Update to new upload-folders
 if [[ -n "$oldUploadPath" && -n "$newUploadPath" ]]; then
+	
 	echo -e "\nUpdating $oldUploadPath to $newUploadPath on all plotters..."
-	for ip in $ips; do
+	echo $ips | while read -r ip; do
 		echo "Updating $ip"
 		ssh -n root@$ip 'nohup zsh ~/chia-plotting-automation/functions/upload.zsh "/$(ls /mnt/everplot*/plot | grep -P -m 1 serial)/" &> ~/nohup.out &'
 	done
+
 fi
 
 # Trigger upload if needed on them
 echo -e "\nRestarting uploads on all plotters..."
-for ip in $ips; do
-	echo "Restart upload on $ip"
+
+echo $ips | while read -r ip; do 
+echo "Restart upload on $ip"
 	ssh -n root@$ip 'nohup zsh ~/chia-plotting-automation/functions/upload.zsh "/$(ls /mnt/everplot*/plot | grep -P -m 1 serial)/" &> ~/nohup.out &'
 done
